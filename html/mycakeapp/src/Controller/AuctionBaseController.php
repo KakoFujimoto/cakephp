5a7e0ae54a6492231aca17ca3847665fdab19ced
Kako Fujimoto <ayaka20111005@yahoo.co.jp>
2020-07-02 15:28:43 +0900

    AuctionControllerの認証処理にRatingsControllerも追加

--- html/mycakeapp/src/Controller/AuctionBaseController.php
+++ html/mycakeapp/src/Controller/AuctionBaseController.php
@@ -76,11 +76,12 @@ class AuctionBaseController extends AppController
 		if ($user['role'] === 'admin') {
 			return true;
 		}
-		// 一般ユーザーはAuctionControllerのみtrue、他はfalse
+		// 一般ユーザーはAuction、Messages、RatingsControllerのみtrue、他はfalse
 		if ($user['role'] === 'user') {
 			if (
 				$this->name == 'Auction' ||
-				$this->name == 'Messages'
+				$this->name == 'Messages' ||
+				$this->name == 'Ratings'
 			) {
 				return true;
 			} else {
