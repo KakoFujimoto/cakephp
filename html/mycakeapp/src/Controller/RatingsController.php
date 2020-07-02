2e9c61f3796d85f6d8704200151040bd392416e6
Kako Fujimoto <ayaka20111005@yahoo.co.jp>
2020-07-02 13:57:28 +0900

    評価フォーム表示部分に変数を渡すことに成功

--- html/mycakeapp/src/Controller/RatingsController.php
+++ html/mycakeapp/src/Controller/RatingsController.php
@@ -1,7 +1,10 @@
 <?php
+
 namespace App\Controller;
 
 use App\Controller\AppController;
+use Cake\Event\Event; // added.
+use Exception; // added.
 
 /**
  * Ratings Controller
@@ -10,60 +13,75 @@ use App\Controller\AppController;
  *
  * @method \App\Model\Entity\Rating[]|\Cake\Datasource\ResultSetInterface paginate($object = null, array $settings = [])
  */
-class RatingsController extends AppController
+class RatingsController extends AuctionBaseController
 {
-    /**
-     * Index method
-     *
-     * @return \Cake\Http\Response|null
-     */
+    // デフォルトテーブルを使わない
+    public $useTable = false;
+
+    public function initialize()
+    {
+
+        parent::initialize();
+        $this->loadComponent('Paginator');
+        // 必要なモデルをすべてロード
+        $this->loadModel('Users');
+        $this->loadModel('Biditems');
+        $this->loadModel('Bidrequests');
+        $this->loadModel('Bidinfo');
+        $this->loadModel('Bidmessages');
+        $this->loadModel('Messages');
+        $this->loadModel('Ratings');
+
+
+        // ログインしているユーザー情報をauthuserに設定
+        $this->set('authuser', $this->Auth->user());
+        // レイアウトをauctionに変更
+        $this->viewBuilder()->setLayout('auction');
+    }
+
     public function index()
     {
         $this->paginate = [
-            'contain' => ['Users', 'RatedUsers', 'Bidinfos'],
+            'contain' => ['Users', 'Bidinfo', 'Biditems'],
         ];
         $ratings = $this->paginate($this->Ratings);
 
         $this->set(compact('ratings'));
     }
 
-    /**
-     * View method
-     *
-     * @param string|null $id Rating id.
-     * @return \Cake\Http\Response|null
-     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-     */
+
+    //  * @param string|null $id Rating id.
+
     public function view($id = null)
     {
         $rating = $this->Ratings->get($id, [
-            'contain' => ['Users', 'RatedUsers', 'Bidinfos'],
+            'contain' => ['Users', 'Bidinfo', 'Biditems'],
         ]);
 
         $this->set('rating', $rating);
     }
 
-    /**
-     * Add method
-     *
-     * @return \Cake\Http\Response|null Redirects on successful add, renders view otherwise.
-     */
-    public function add()
+    public function add($id = null)
     {
-        $rating = $this->Ratings->newEntity();
-        if ($this->request->is('post')) {
-            $rating = $this->Ratings->patchEntity($rating, $this->request->getData());
-            if ($this->Ratings->save($rating)) {
-                $this->Flash->success(__('The rating has been saved.'));
+        // bidinfoのidが$idと一致するものだけをadd.ctpに渡す
+        $bidinfo = $this->Bidinfo->get($id);
+        // biditemsのidがbidinfoのbiditem_idと一致するものをadd.ctpに渡す
+        $biditems = $this->Biditems->get($bidinfo->biditem_id);
 
-                return $this->redirect(['action' => 'index']);
-            }
-            $this->Flash->error(__('The rating could not be saved. Please, try again.'));
-        }
-        $users = $this->Ratings->Users->find('list', ['limit' => 200]);
-        $ratedUsers = $this->Ratings->RatedUsers->find('list', ['limit' => 200]);
-        $bidinfos = $this->Ratings->Bidinfos->find('list', ['limit' => 200]);
-        $this->set(compact('rating', 'users', 'ratedUsers', 'bidinfos'));
+        //saveの処理
+        // $rating = $this->Ratings->newEntity();
+        // if ($this->request->is('post')) {
+        //     $rating = $this->Ratings->patchEntity($rating, $this->request->getData());
+        //     if ($this->Ratings->save($rating)) {
+        //         $this->Flash->success(__('The rating has been saved.'));
+
+        //         return $this->redirect(['action' => 'index']);
+        //     }
+        //     $this->Flash->error(__('The rating could not be saved. Please, try again.'));
+        // }
+
+
+        $this->set(compact('bidinfo', 'biditems'));
     }
 
     /**
@@ -76,7 +94,7 @@ class RatingsController extends AppController
     public function edit($id = null)
     {
         $rating = $this->Ratings->get($id, [
-            'contain' => [],
+            'contain' => ['Users', 'Bidinfo', 'Biditems'],
         ]);
         if ($this->request->is(['patch', 'post', 'put'])) {
             $rating = $this->Ratings->patchEntity($rating, $this->request->getData());
@@ -89,8 +107,8 @@ class RatingsController extends AppController
         }
         $users = $this->Ratings->Users->find('list', ['limit' => 200]);
         $ratedUsers = $this->Ratings->RatedUsers->find('list', ['limit' => 200]);
-        $bidinfos = $this->Ratings->Bidinfos->find('list', ['limit' => 200]);
-        $this->set(compact('rating', 'users', 'ratedUsers', 'bidinfos'));
+        $bidinfo = $this->Ratings->Bidinfo->find('list', ['limit' => 200]);
+        $this->set(compact('rating', 'users', 'ratedUsers', 'bidinfo'));
     }
 
     /**
--- html/mycakeapp/src/Template/Ratings/add.ctp
+++ html/mycakeapp/src/Template/Ratings/add.ctp
@@ -1,27 +1,22 @@
-<?php
-/**
- * @var \App\View\AppView $this
- * @var \App\Model\Entity\Rating $rating
- */
-?>
-<nav class="large-3 medium-4 columns" id="actions-sidebar">
+<!-- <nav class="large-3 medium-4 columns" id="actions-sidebar">
     <ul class="side-nav">
         <li class="heading"><?= __('Actions') ?></li>
         <li><?= $this->Html->link(__('List Ratings'), ['action' => 'index']) ?></li>
         <li><?= $this->Html->link(__('List Users'), ['controller' => 'Users', 'action' => 'index']) ?></li>
         <li><?= $this->Html->link(__('New User'), ['controller' => 'Users', 'action' => 'add']) ?></li>
     </ul>
-</nav>
+</nav> -->
 <div class="ratings form large-9 medium-8 columns content">
     <?= $this->Form->create($rating) ?>
     <fieldset>
-        <legend><?= __('Add Rating') ?></legend>
+        <legend><?= __('取引相手を評価する') ?></legend>
         <?php
-            echo $this->Form->control('user_id', ['options' => $users]);
-            echo $this->Form->control('rated_user_id');
-            echo $this->Form->control('stars');
-            echo $this->Form->control('comments');
-            echo $this->Form->control('bidinfo_id');
+        echo $this->Form->hidden('bidinfo_id', ['value' => $bidinfo->id]);
+        echo $this->Form->hidden('user_id', ['value' => $bidinfo->user_id]);
+        echo $this->Form->hidden('rated_user_id', ['value' => $biditems->user_id]);
+        echo $this->Form->control('stars');
+        echo $this->Form->control('comments');
+        var_dump($biditems->user_id);
         ?>
     </fieldset>
     <?= $this->Form->button(__('Submit')) ?>
